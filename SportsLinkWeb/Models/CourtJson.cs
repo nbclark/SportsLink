86276e0f92a0f045fc5b16861ee5899ef0ed9b30
Nicholas Clark <nbclark@gmail.com>
2011-05-18 10:09:54 -0700

    adding court json

---
+++ SportsLinkWeb/Models/CourtJson.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using SportsLink;
+
+namespace SportsLinkWeb.Models
+{
+    public class CourtJson
+    {
+        public string id;
+        public string name;
+        public float latitude;
+        public float longitude;
+
+        public Guid GuidId
+        {
+            get
+            {
+                return new Guid(id.Substring(0, 32));
+            }
+        }
+    }
+}
